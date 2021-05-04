e5ed4d483fb05faa5e826df760bc895a2b39099f
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-05-03 23:47:40 -0300

    signIn structures done

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import Header from './components/header/header.component.jsx'
+import Header from './components/header/header.component'
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component';
+import signInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 
 
@@ -16,6 +17,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
+        <Route exact path='/signin' component={signInAndSignUpPage} />
       </Switch>
     </div>
   );
