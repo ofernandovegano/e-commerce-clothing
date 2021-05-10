ed987f7e46151581e1969ac2a3b02a0d965b29e0
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-05-10 17:34:01 -0300

    shop state refactored to a reducer

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,19 +1,12 @@
-import React, { Component } from 'react';
-import SHOP_DATA from './shop.data.js'
+import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+import { selectCollections } from '../../redux/shop/shop.selectors';
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-  render() {
-    const { collections } = this.state
-    return (
+const ShopPage = ({collections}) => (
       <div className='shop-page'>
         {
           collections.map(({id, ...otherCollectionProps}) => (
@@ -21,8 +14,10 @@ class ShopPage extends Component {
           ))
         }
       </div>
-    )
-  }
-}
+);
 
-export default ShopPage;
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections
+});
+    
+export default connect(mapStateToProps)(ShopPage);
