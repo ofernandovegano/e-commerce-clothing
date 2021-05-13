240913c4beb2f88d0eeaf4278aa93f0e5311620a
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-05-13 12:51:25 -0300

    make changes to make posible to get data from the database

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -4,11 +4,27 @@ import { Route } from 'react-router-dom';
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component'
 
-const ShopPage = ({ match }) => (
-  <div className='shop-page'>
-    <Route exact path={`${match.path}`} component={CollectionOverview} />
-    <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
-  </div>
-);
+import { firestore, coventCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
+
+class ShopPage extends React.Component {
+  unsubscribeFromSnapshot = null;
+
+  componentDidMount() {
+    const collectionRef = firestore.collection('collections');
+    collectionRef.onSnapshot(async snapshot => {
+      coventCollectionsSnapshotToMap(snapshot);
+    })
+  }
+
+  render() {
+    const {match} = this.props;
+    return (
+      <div className='shop-page'>
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      </div>
+    );
+  }
+};
     
 export default ShopPage;
