2a0218e72c7cdc8e06ebafea02455f2c5cba9d86
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-05-13 15:52:07 -0300

    transfer data from database to Redux State

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -51,17 +51,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
 };
 
 export const coventCollectionsSnapshotToMap = (collections) => {
-  const transformedCollection = collections.docs.map(doc => {
-    const { title, items } = doc.data();
+  const transformedCollection = collections.docs.map(docSnapshot => {
+    const { title, items } = docSnapshot.data();
 
     return {
       routeName: encodeURI(title.toLowerCase()),
-      id: doc.id,
+      id: docSnapshot.id,
       title,
       items
     }
   });
-  console.log(transformedCollection);
+
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {});
 };
 
 firebase.initializeApp(config);
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-
-import CollectionOverview from '../../components/collections-overview/collections-overview.component';
-import CollectionPage from '../collection/collection.component'
+import { connect } from 'react-redux';
 
 import { firestore, coventCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
+import { updateCollections } from '../../redux/shop/shop.actions';
+  
+import CollectionOverview from '../../components/collections-overview/collections-overview.component';
+import CollectionPage from '../collection/collection.component'
+
+
 class ShopPage extends React.Component {
   unsubscribeFromSnapshot = null;
-
+  
   componentDidMount() {
+    const { updateCollections } = this.props;
     const collectionRef = firestore.collection('collections');
     collectionRef.onSnapshot(async snapshot => {
-      coventCollectionsSnapshotToMap(snapshot);
+      const collectionsMap = coventCollectionsSnapshotToMap(snapshot);
+      updateCollections(collectionsMap);
     })
   }
 
@@ -26,5 +32,10 @@ class ShopPage extends React.Component {
     );
   }
 };
+
+const mapDispatchToProps = dispatch => ({
+  updateCollections: collectionsMap =>
+    dispatch(updateCollections(collectionsMap))
+});
     
-export default ShopPage;
+export default connect(null, mapDispatchToProps)(ShopPage);
