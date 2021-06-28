d7c1862faaab85d9ea314a46a5ee12b85771c02f
Fernando Gomes <gomesrodriguesfernando@gmail.com>
2021-06-27 21:31:45 -0300

    add feature to save cart in DB. When user logout and login the cart he/she left come back

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -12,6 +12,8 @@ const config = {
   measurementId: "G-E8KNKVTPFP"
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -36,6 +38,19 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
 
   return userRef
+};
+
+export const getUserCartRef = async (userId) => {
+  const cartsRef = firestore.collection("carts").where("userId", "==", userId);
+  const snapShot = await cartsRef.get();
+
+  if (snapShot.empty) {
+    const cartDocRef = firestore.collection("carts").doc();
+    await cartDocRef.set({ userId, cartItems: [] });
+    return cartDocRef;
+  } else {
+    return snapShot.docs[0].ref;
+  }
 };
 
 export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
@@ -67,8 +82,6 @@ export const coventCollectionsSnapshotToMap = (collections) => {
     return accumulator;
   }, {});
 };
-
-firebase.initializeApp(config);
 
 export const getCurrentUser = () => {
   return new Promise((resolve, reject) => {
